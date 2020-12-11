298464521c617f94ec07a83eb5abbbb9f9407c97
kristofegghe <s0172975@ad.ua.ac.be>
2020-12-11 17:05:25 +0100

    update file to look more professional documentation, main where u can initialize parameters

--- pageRank.py
+++ pageRank.py
@@ -5,122 +5,177 @@ import shutil
 from operator import add
 
 
-def computeRankScore(outlinks, PR, di):
-    totalOutlinks = len(outlinks)
+def computeRankScore(outlinks, PR):
 
     # yield is de emit in de slides, das eigenlijk een return die de toestand van de functie bijhoudt en de volgende keer start bij yield in plaats van het begin van de functie
     for link in outlinks:
-        yield (link, PR / totalOutlinks)
+        yield (link, PR / len(outlinks))
 
-try:
-    shutil.rmtree('ranks.csv')
-except:
-    pass
-try:
-    os.remove('pagerank_list.csv')
-except:
-    pass
-sc = SparkContext("local[*]", "first app")
-print(sc.uiWebUrl)
-start_of_program=time.time()
-logFile="web-Google.txt"
-rd=sc.textFile(logFile)
-# maak een RDD met values 0 .... en indexen die de edges weergeven van het voorbeeld in de slides (yahoo amazon en microsoft)
-# rd = sc.parallelize(["0 2 6 7 8 9 10 1","" ,"0 2 3", "2 5"]).zipWithIndex()
-# switch de values en keys zodat de keys de page zelf is en de values de edges
 
-#initial maps en group om de data in juist formaat te krijgen (id , lijst van IDS)
-rd = rd.map(lambda x: (int(x.split("\t")[0]),int(x.split("\t")[1])))
-values=rd.values().distinct().map(lambda x:(x,x))
-rd = rd.union(values).distinct()
-rd = rd.groupByKey()
-rd = rd.mapValues(lambda x: (list(x))).cache()
-# krijg de N voor de formules
-n=rd.count()
-print(n)
-# initializeer alle rank scores op 1/n
-ranks = rd.mapValues(lambda x: 1.0 / n)
-# meer prints voor duidelijkheid van wat er is aant gebeuren
-# print(rd.collect())
-# print(ranks.collect())
-# print(rd.join(ranks).collect())
-# initializeer waardes die gebruikt worden in formules b = teleportwaarde Beta
-b = 0.5
-epsilon = 1e-6
 
-# initializeer val > 1 en iteratie count op 0 en previousrank ri op de 1/n rank scores
-val = 2
-iter = 0
-ri = ranks
+# functie die word gebruikt als we iets gaan doen met sink nodes word nu nog niet echt gebruikt
+def addifin(list, nummer):
+    if nummer in list:
+        return list
+    else:
+        return list + [nummer]
 
-# blijf loopen zolang de epsilon waarde niet is bereikt
-# epsilon is het verschil in rankscores nu en vorige iteratie
-# er zijn 3 normen die hier staan N0= maximum fout  N1= som van absolute fouten N2= wortel van alle fouten **2
-start_time=time.time()
-timetable={}
-while val > epsilon:
 
-    iter += 1
+#start the pageranking algorithm with the values in order
+#filename, we support web-Google.txt and ClueWeb09_WG format
+#beta, the probabibility to not teleport
+#epsilon, value for convergence calculations
+#allnorms, True means 3 norms will be calculated and a timetable will be generated, False means only Ninf norm will be used and no timetable will be generated
+#commonsolution,True means using the backlink method to fix sinknodes, false means giving every node a link to itself, if it doesn't have one already
 
-    # join rd en ranks zodat de links ranks en scores samen in een RDD zitten en mappen de scores in een nieuwe RDD
-    startContribs=time.time()
-    contribs = rd.join(ranks).flatMap(
-        lambda url_ranks: computeRankScore(url_ranks[1][0], url_ranks[1][1], url_ranks[0]))
+def pageRank(filename,beta,epsilon,allnorms=False,commonsolution=False):
+    #initialize the RDDs in the correct formats, (ID, OUTGOINGLINKS)
+    #initial maps en group om de data in juist formaat te krijgen (id , lijst van outgoing links)
+    if commonsolution:
+        #common solution is making backlinks for all sink nodes
+        if filename=="web-Google.txt":
+            rd = sc.textFile(filename)
+            rd = rd.map(lambda x: (int(x.split("\t")[0]),int(x.split("\t")[1])))
+            rd = rd.groupByKey().cache()
+            rd=rd.mapValues(lambda x:list(x))
+        else:
+            rd = sc.textFile(filename).zipWithIndex()
+            rd = rd.map(
+                lambda x: (x[1], list(filter(lambda z: z < 50e6, [int(z) for z in x[0].split()]))).filter(
+                lambda x: x[0] < 50e6))
 
-    # reducebykey(add) telt alles values die dezelfde keys hebben bij elkaar op en de map values past de formule van random teleports toe op deze score en update de nieuwe ranksscores
-    ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * b + (1 - b) * 1 / n)
-    # bereken alle normen die nodig zijn met reduce en map functies.
-    diff = ranks.join(ri).mapValues(lambda rank: abs(rank[0] - rank[1]))
-    diffsquared=diff.mapValues(lambda rank:rank**2)
+        #initialize incoming links for sink nodes
+        m2=rd.flatMapValues(lambda x:x).map(lambda x:(x[1],x[0])).groupByKey()
+        m2=m2.mapValues(lambda x:list(x))
 
-    print("diff", time.time() - startContribs)
-    ninf = diff.values().max()
-    n1=diff.values().sum()
-    n2=diffsquared.values().reduce(add) ** 0.5
-    val=n1
-    timetable[iter]=[ninf,n1,n2]
-    print("NORM N1:", n1)
-    print("NORM Ninf:", ninf)
-    print("NORM N2:", n2)
-    # sla de huidige ranks op in de oude ri.
+        # maak backlinks voor alle sink nodes
+        rd3=rd.fullOuterJoin(m2)
+        rd3=rd3.filter(lambda x: x[1][0]==None).map(lambda x: (x[0],x[1][1]))
 
+        rd=rd.union(rd3)
+    else:
+        # testsolution is making spidertraps out of all sink nodes
+        if filename=="web-Google.txt":
+            rd = sc.textFile(filename)
+            rd = rd.map(lambda x: (int(x.split("\t")[0]),int(x.split("\t")[1])))
+            values=rd.values().distinct().map(lambda x:(x,x))
+            rd = rd.union(values).distinct()
+            rd = rd.groupByKey()
+            rd = rd.mapValues(lambda x: (list(x))).cache()
+        else:
+            rd = sc.textFile(filename).zipWithIndex()
+            rd = rd.map(lambda x: (
+            x[1], list(filter(lambda z: z < 50e6, addifin([int(z) for z in x[0].split()], x[1]))))).filter(
+                lambda x: x[0] < 50e6)
+    # get N used in formulas
+    n=rd.count()
+    print(n)
+    # initialize all ranks on 1/n
+    ranks = rd.mapValues(lambda x: 1.0 / n)
+    # initialize value above 1, iterations at 0 and the previous ranks ri to the ranks we just initialized
+    val = 2
+    iter = 0
     ri = ranks
-    time2=time.time()
-    print("iteration {} duurt ".format(iter),time2-start_time)
-    start_time=time2
 
-#sorteer op value, coalesce zorgt ervoor dat er 1 partitie is false= shuffling=false zodat er niets geshuffled word
-print("iteration {} zorgt voor een epsilon van {}".format(iter,epsilon))
-#schrijf naar csv
-lines=ranks.coalesce(1,False).sortBy(lambda a:a[1],False,1)
-# nogmaals coalesce voor de zekerheid en save to ranks.csv/part0000 (geen idee waarom ik niet rechtstreeks naar een file kan schrijven :( )
-lines.coalesce(1,False).saveAsTextFile("ranks.csv")
+    #keep looping aslong as the difference between the former ranks and the current ranks is bigger than epsilon
+    #bigger means that the norm of the residual vector abs(ri-ranks).
+    # we test for 3 norms, Ninf= max(vector) N1 = sum(vector) N2= root of sum of all elements squared
+    #time table and timer variables are initialized aswell
+    start_time=time.time()
+    timetable={}
+    while val > epsilon:
 
-#store a timetable of norms and a ratios of norm to check for growth rates
-stringske="timetable : \n"
-stringratios="ratios :\n"
-t=[1,1,1]
-for i in timetable:
-    string2="time {} {} \n".format(i,timetable[i])
-    stringratios+="time {} {}\n".format(i,[timetable[i][0]/t[0],timetable[i][1]/t[1],timetable[i][2]/t[2]])
-    t=timetable[i]
-    stringske+=string2
+        iter += 1
+        # join rd and ranks to get links together with scores in the same RDD and use flatmap to calculate all contributions
+        # to the pagerank score with our generator function
+        # the map and reduce function are based on https://www.coursera.org/lecture/big-data-analysis/rdd-implementation-F3Y0c
+        # this is a course on efficient pageranking with RDDs and is
+        # the operations match the map and reduce operations given in the lectures very well, so we used it.
+        startContribs=time.time()
+        contribs = rd.join(ranks).flatMap(
+            lambda url_ranks: computeRankScore(url_ranks[1][0], url_ranks[1][1]))
 
-a=open("timetable.txt","w")
-r=open("ratios.txt","w")
-a.write(stringske)
-r.write(stringratios)
-a.close()
-r.close()
-#probeer alle gemaakte files op de juiste plaats te zetten en verwijder de ranks.csv directory
-try:
-    shutil.move("ranks.csv/part-00000", "pagerank_list.csv")
+        # contribs now contains a list of tuples in the form of (ID, score) reducebykey will add all these values, this is the summation in the formula
+        # the mapvalues will use adjust the rank with the formula rank= rank*b + a/n
+
+        ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * beta + (1 - beta) * 1.0 / n)
+
+        # calculate the norms we will use and print the results.
+        diff = ranks.join(ri).mapValues(lambda rank: abs(rank[0] - rank[1]))
+        ninf = diff.values().max()
+        val = ninf
+        if allnorms:
+            diffsquared=diff.mapValues(lambda rank:rank**2)
+            n1=diff.values().sum()
+            n2=diffsquared.values().reduce(add) ** 0.5
+            timetable[iter]=[ninf,n1,n2]
+            print("NORM N1:", n1)
+            print("NORM Ninf:", ninf)
+            print("NORM N2:", n2)
+        else:
+            print("NORM Ninf:", ninf)
+
+        # set old ranks equal to the calculated ranks and print the time it took for current iteration
+        ri = ranks
+        time2=time.time()
+        print("iteration {} duurt ".format(iter),time2-start_time)
+        start_time=time2
+
+    print("iteration {} zorgt voor een epsilon van {}".format(iter,epsilon))
+    #sort the values with a coalesce zo there is only one partition and one file
+    lines=ranks.coalesce(1,False).sortBy(lambda a:a[1],False,1)
+    # coalesce once more and write to ranks.csv, this generates a directory instead of a file so we will move the file later.
+    lines.coalesce(1,False).saveAsTextFile("ranks.csv")
+    # generate a timetable if comparing all norms
+    if allnorms:
+        stringske="timetable : \n"
+        stringratios="ratios :\n"
+        t=[1,1,1]
+        for i in timetable:
+            string2="time {} {} \n".format(i,timetable[i])
+            stringratios+="time {} {}\n".format(i,[timetable[i][0]/t[0],timetable[i][1]/t[1],timetable[i][2]/t[2]])
+            t=timetable[i]
+            stringske+=string2
+
+        a=open("timetable.txt","w")
+        r=open("ratios.txt","w")
+        a.write(stringske)
+        r.write(stringratios)
+        a.close()
+        r.close()
+
+if __name__ == "__main__":
+    # remove all files that will be generated again
     try:
         shutil.rmtree('ranks.csv')
     except:
         pass
+    try:
+        os.remove('pagerank_list.csv')
 
-except:
-    print('U can find it in the ranks.csv directory under part-00000')
-print("total time of program: ",time.time()-start_of_program)
 
+    except:
+        pass
+    # start sparkcontext with all cores and print weburl where u can monitor spark and start a timer for the program.
+    sc = SparkContext("local[*]", "first app")
+    print(sc.uiWebUrl)
+    start_of_program=time.time()
+    #start the pageranking algorithm with the values in order
+    #filename, we support web-Google.txt and ClueWeb09_WG format
+    #beta, the probabibility to not teleport
+    #epsilon, value for convergence calculations
+    #allnorms, True means 3 norms will be calculated and a timetable will be generated, False means only Ninf norm will be used and no timetable will be generated
+    #commonsolution,True means using the backlink method to fix sinknodes, false means giving every node a link to itself, if it doesn't have one already
+    pageRank("web-Google.txt",0.85,1e-6,True,True)
+
+    # move and remove the files that are not needed anymore and print the total time it took for the program to run.
+    try:
+        shutil.move("ranks.csv/part-00000", "pagerank_list.csv")
+        try:
+            shutil.rmtree('ranks.csv')
+        except:
+            pass
+
+    except:
+        print('U can find it in the ranks.csv directory under part-00000')
+    print("total time of program: ", time.time() - start_of_program)
